daeb15588631429800ebd072a95845918582a727
MartinCerman <cermanm@gmail.com>
2024-02-09 19:16:07 +0100

    Added links for file attachments.

--- app/Components/TasksTableControl.php
+++ app/Components/TasksTableControl.php
@@ -5,14 +5,30 @@ declare(strict_types=1);
 namespace App\Components;
 
 use App\Models\TasksTemplate;
+use App\Models\UploadsRepository;
 use Nette\Application\UI\Control;
 
 /** @property-read TasksTemplate $template */
 class TasksTableControl extends Control
 {
+    public function __construct(private UploadsRepository $uploadsRepository)
+    {
+    }
+
     public function render(array $tasks): void
     {
         $this->template->tasks = $tasks;
         $this->template->render(__DIR__ . '/TasksTableTemplate.latte');
+    }
+
+    public function getTaskFile($taskId) : ?string
+    {
+        $file = $this->uploadsRepository->findFile((string)$taskId);
+
+        if($file){
+            return $file->getFilename();
+        }
+
+        return null;
     }
 }
