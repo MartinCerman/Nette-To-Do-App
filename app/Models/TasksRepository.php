05744e9d0cafbedd86ce6335a7becc6c08cc9846
MartinCerman <cermanm@gmail.com>
2024-02-15 20:16:17 +0100

    Refactor, presenter now uses nettrine instead of nette db explorer.

--- app/Models/TasksRepository.php
+++ app/Models/TasksRepository.php
@@ -10,12 +10,12 @@ use Nette\Database\Explorer;
 use Nette\Database\Table\ActiveRow;
 use Nette\Database\Table\Selection;
 use Doctrine\DBAL\Connection;
+use Nette\Utils\ArrayHash;
 use Nettrine\ORM\EntityManagerDecorator;
 
 class TasksRepository
 {
     public function __construct(
-        private Explorer $database,
         private EntityManagerDecorator $entityManager)
     {
     }
@@ -39,13 +39,15 @@ class TasksRepository
 
     public function addTask($name, $description): int
     {
-        $insertedRow = $this->database->table('tasks')
-            ->insert([
-                'name' => $name,
-                'description' => $description
-            ]);
+        $task = new Task();
+        $task->setName($name);
+        $task->setDescription($description);
 
-        return $insertedRow->id;
+        $this->entityManager->persist($task);
+
+        $this->entityManager->flush();
+
+        return $task->getId();
     }
 
     public function getById(int $taskId): Task
@@ -53,17 +55,30 @@ class TasksRepository
         return $this->entityManager->find(Task::class, $taskId);
     }
 
-    public function updateTask(array $taskData): void
+    public function updateTask(int $taskId, array $taskData): void
     {
-        $this->database->table('tasks')
-            ->get($taskData['id'])
-            ?->update($taskData);
+        $task = $this->entityManager->find(Task::class, $taskId);
+
+        if ($task === null) {
+            throw new \InvalidArgumentException("Task with id '{$taskId}' not found.");
+        }
+
+        foreach ($taskData as $property => $value) {
+            $setter = 'set' . ucfirst($property);
+            if (method_exists($task, $setter)) {
+                $task->$setter($value);
+            } else {
+                throw new \InvalidArgumentException("Property $property not found.");
+            }
+        }
+
+        $this->entityManager->flush();
     }
 
     public function removeTask(int $taskId): void
     {
         $task = $this->entityManager->find(Task::class, $taskId);
-        if($task){
+        if ($task) {
             $this->entityManager->remove($task);
             $this->entityManager->flush();
         }
